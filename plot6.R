e283e18fa863611581dbd057c6f5284fe7df4e1a
Andry Herizo RAJOELISON <finalherizo@gmail.com>
2020-11-26 21:51:01 +0300

    Added plot6

--- plot6.R
+++ plot6.R
@@ -24,26 +24,25 @@ motor_vehicles_sectors <- scc %>%
 #        select(SCC) %>%
 #        mutate(SCC = as.character(SCC))
 
-BaltimoreMotorVehicleEmissions <- nei %>%
-        filter(nei$fips == "24510", SCC %in% motor_vehicles_sectors$SCC) %>%
+MotorVehicleEmissions <- nei %>%
+        filter(nei$fips == "24510" | nei$fips == "06037", SCC %in% motor_vehicles_sectors$SCC) %>%
         mutate(Sector = motor_vehicles_sectors$EI.Sector[match(SCC, motor_vehicles_sectors$SCC)]) %>%
-        group_by(year, Sector) %>% summarise(Emissions = sum(Emissions))
+        group_by(year, fips) %>% summarise(Emissions = sum(Emissions))
 
-plot <- ggplot(BaltimoreMotorVehicleEmissions, aes(year, Emissions)) +
-        geom_point(size = 2, aes(color=factor(Sector), shape=factor(Sector))) +
-        geom_smooth(aes(group = BaltimoreMotorVehicleEmissions$Sector, color=factor(BaltimoreMotorVehicleEmissions$Sector)), 
+plot <- ggplot(MotorVehicleEmissions, aes(year, Emissions)) +
+        geom_point(size = 2, aes(color=factor(fips))) +
+        geom_smooth(aes(group = MotorVehicleEmissions$fips, color=factor(MotorVehicleEmissions$fips)), 
                     method = "lm", formula = y ~ x, fill = NA, lty=2, lwd=0.4) +
-        labs(title = "Emissions from motor vehicles related sources in the Baltimore City.") +
+        labs(title = "Emissions from motor vehicles in the Baltimore City and Los Angeles County.") +
         xlab("Year") +
-        scale_color_discrete(name = "Sector", labels = factor(BaltimoreMotorVehicleEmissions$Sector)) +
-        scale_shape_discrete(name = "Sector", labels = factor(BaltimoreMotorVehicleEmissions$Sector)) +
+        scale_color_discrete(name = "County", labels = c("Los Angeles County", "Baltimore City")) +
         theme(plot.title = element_text(hjust = 0.5)) #+
         #theme(legend.position = "bottom")
         
         
 print(plot)
 
-dev.copy(png, "plot5.png")
+dev.copy(png, "plot6.png")
 dev.off()
 
 setwd(wd)
